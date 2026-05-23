b84e0af0b1562a8aef52212b46d21e7d28789179
Gaurav Suryawanshi <kundaliksofficial@gmail.com>
2026-05-23 10:46:55 +0530

    remove-ui-from-library

--- app/src/main/java/com/kor/genericprinter/listeners/OnItemClickListener.kt
+++
@@ -1,6 +0,0 @@
-package com.kor.genericprinter.listeners
-
-interface OnItemClickListener {
-    fun onItemClick(position: Int)
-
-}
--- app/src/main/java/com/kor/genericprinter/models/BluetoothDeviceData.kt
+++
@@ -1,9 +0,0 @@
-package com.kor.genericprinter.models
-
-data class BluetoothDeviceData(
-
-    val name: String? = null,
-    val address: String? = "00.00.00.00.00.00",
-    val status: String? = null
-
-)
